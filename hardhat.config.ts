e5f60f26fd2ba005ea6dc8ee1cbdf99686515739
aqilusman45 <aqilusman45@gmail.com>
2022-08-20 17:33:34 -0500

    feat: configure multiple compiler for solidity

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,26 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-solhint";
-import "hardhat-deploy";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "@nomiclabs/hardhat-solhint"
+import "hardhat-deploy"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
-};
+    // solidity: "0.8.9",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+            {
+                version: "0.6.6",
+            },
+        ],
+    },
+    defaultNetwork: "hardhat",
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+        },
+    },
+}
 
-export default config;
+export default config
